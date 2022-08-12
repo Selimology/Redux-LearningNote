56b0621e2c427ecf8f3cb3c47bf52b184321440e
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-08-12 21:42:06 +0200

    Shopping Cart , useSelector , Slice & Store

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux';
+function Navbar() {
+  /* Returns all the reducers in store
+  console.log(
+    useSelector((store) => {
+      console.log(store);
+    })
+  ); */
+
+  /*   you may also deconstruct this:
+  const amount = useSelector((store) => store.cart.amount); */
+
+  const { amount } = useSelector((store) => store.cart);
+  return (
+    <>
+      <nav>
+        <div className="nav-center">
+          <h3>Redux Toolkit</h3>
+          <div className="nav-container">
+            <CartIcon />
+            <div className="amount-container">
+              <p className="total-amount">{amount}</p>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
