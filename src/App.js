3ba23e381b41f1976b2aafc45a4baf4162fb2946
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-08-13 08:59:40 +0200

    Shopping cart website w/ pop to confirm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotal } from './features/cart/cartSlice';
+import { useEffect } from 'react';
+import Modal from './components/Modal';
+
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,14 +3,56 @@ import cartItems from '../../cartItems';
 
 const initialState = {
   cartItems: cartItems,
-  amount: 0,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+
+      /* you can create a whole new state
+      return {}
+      will replace the initialStatate to empty object
+      */
+      /* return { cartItems: [] };
+      This replaced the whole initialState to ={
+        cartItems:[]
+      }
+      
+      */
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
 });
 
 //it is an object, reducer is property
 export default cartSlice.reducer;
+
+//export reducer function = Slice Name + .action
+export const { clearCart, removeItem, increase, decrease, calculateTotal } =
+  cartSlice.actions;
